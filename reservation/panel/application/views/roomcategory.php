6bf4a54e167c5badd1ccac0d9e9479cbc557492c
E-Sahra-B <bendenizsahra@gmail.com>
2023-09-21 00:35:59 +0300

    RoomCategoryRefactoring

--- reservation/panel/application/views/roomcategory.php
+++ reservation/panel/application/views/roomcategory.php
@@ -23,6 +23,22 @@
     </div>
     <!-- ./wrapper -->
     <?php $this->load->view("includes/include_script"); ?>
+    <script>
+        $(document).ready(function() {
+            var base_url = $(".base_url").text();
+            // Bootstrap Toggle init
+            $('.toggle_check').bootstrapToggle();
+            // isActive Change
+            $('.toggle_check').change(function() {
+                var isActive = $(this).prop('checked');
+                var id = $(this).attr("dataID");
+                $.post(base_url + "roomcategory/isActiveSetter", {
+                    id: id,
+                    isActive: isActive
+                }, function(response) {});
+            })
+        })
+    </script>
 </body>
 
 </html>
